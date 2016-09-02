70d40e9357e4c90e9c040813d8c3eaa717123a76
TheeGrassHopper <almortada88@gmail.com>
2016-09-02 15:01:28 -0500

    added sound

--- main.js
+++ main.js
@@ -4,62 +4,84 @@ $(document).ready( function() {
 
     $(".yeti").mousedown(function() {
     	if($('.yeti').addClass('yetiOut')){
-    		$(this).removeClass('yeti').addClass('jqu');
+    		$(this).removeClass('yeti').addClass('yetiOut');
     	}
-        alert("Yaaaarrrr!");;
-        window.location.reload()
+
+    	var aNote = document.getElementById('aAudio');
+		aNote.currentTime = 0;
+		aNote.play();
+        alert("Yaaaarrrr!");
+        window.location.reload();
     });
+
+    
 
     $( '.pinguin1' ).click( function() {
         if($( '.pinguin1 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin1').addClass('black');
-
         }
+        var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
 
     $( '.pinguin2' ).click( function() {
         if($( '.pinguin2 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin2').addClass('black2');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
 
     $( '.pinguin3' ).click( function() {
         if($( '.pinguin3 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin3').addClass('black3');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
 
     $( '.pinguin4' ).click( function() {
         if($( '.pinguin4 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin4').addClass('black4');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
     $( '.pinguin5' ).click( function() {
         if($( '.pinguin5 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin5').addClass('black4');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
     $( '.pinguin6' ).click( function() {
         if($( '.pinguin6 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin6').addClass('black4');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
     $( '.pinguin7' ).click( function() {
         if($( '.pinguin7 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin7').addClass('black4');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
     $( '.pinguin8' ).click( function() {
         if($( '.pinguin8 ' ).addClass( 'black')){
         	$(this).removeClass('pinguin8').addClass('black4');
-
         }
+         var aNote1 = document.getElementById('aAudio1');
+		aNote1.currentTime = 0;
+		aNote1.play();
     });
 
 });
